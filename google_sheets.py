34857d05b1898433f583b379ae13b1f1be93bb42
yuliya-droid <yulboyko.85@gmail.com>
2024-07-14 15:22:12 +0300

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -1,24 +1,26 @@
 # google_sheets.py
 
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-
 def book_kayak_in_google_sheets(date, time, kayak_type, quantity):
-    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('path_to_your_service_account_key.json', scope)
-    gc = gspread.authorize(credentials)
+    try:
+        scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name('path_to_your_service_account_key.json', scope)
+        gc = gspread.authorize(credentials)
 
-    spreadsheet_id = '1Tw6GGkbsTkv9RsI_9yJ5t2BxFgrJ6KdQV-W1exhBNFQ'
-    sheet = gc.open_by_key(spreadsheet_id).worksheet('Admin')
+        spreadsheet_id = '1Tw6GGkbsTkv9RsI_9yJ5t2BxFgrJ6KdQV-W1exhBNFQ'
+        sheet = gc.open_by_key(spreadsheet_id).worksheet('Admin')
 
-    records = sheet.get_all_records()
+        records = sheet.get_all_records()
 
-    for i, record in enumerate(records):
-        if record['Дата'] == date and record['Время'] == time:
-            if kayak_type == 'Одноместный каяк':
-                sheet.update_cell(i + 2, 3, quantity)
-            elif kayak_type == 'Двухместный каяк':
-                sheet.update_cell(i + 2, 4, quantity)
-            elif kayak_type == 'САП':
-                sheet.update_cell(i + 2, 5, quantity)
-            break
+        for i, record in enumerate(records):
+            if record['Дата'] == date and record['Время'] == time:
+                if kayak_type == 'Одноместный каяк':
+                    sheet.update_cell(i + 2, 3, quantity)
+                elif kayak_type == 'Двухместный каяк':
+                    sheet.update_cell(i + 2, 4, quantity)
+                elif kayak_type == 'САП':
+                    sheet.update_cell(i + 2, 5, quantity)
+                break
+        else:
+            print("Запись не найдена для указанной даты и времени.")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
